09eda30574ad78afc4a1663c7490119b59725672
Sambhav Jain <sambhav.jiit@gmail.com>
2017-02-27 15:14:27 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	url : 'mongodb://localhost:27017/nearbydb',
+// 	url : 'mongodb://localhost:27017/nearbydb',
 	
-// 	url : 'mongodb://localhost:27017/nearbyDb'
+	url : 'mongodb://localhost:27017/nearbyDb'
 }
